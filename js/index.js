517e1f91ed536a1b90284ceeb48a0da0ae121717
mishaplx <olegorlov100oleg@gmail.com>
2021-07-28 10:05:37 +0200

    add video player

---
+++ js/index.js
@@ -0,0 +1,12 @@
+const progress = document.querySelector('.progress');
+  
+progress.addEventListener('input', function() {
+  const value = this.value;
+  this.style.background = `linear-gradient(to right, #24809E 0%, #24809E ${value}%, #fff ${value}%, white 100%)`
+})
+const progress = document.querySelector('.value__int');
+  
+progress.addEventListener('input', function() {
+  const value = this.value;
+  this.style.background = `linear-gradient(to right, #24809E 0%, #24809E ${value}%, #fff ${value}%, white 100%)`
+})
