9263b3bf6239647b2ea6f2c86e54ff2936f7456f
mishaplx <olegorlov100oleg@gmail.com>
2021-07-31 17:26:09 +0300

    end job

--- js/index.js
+++ js/index.js
@@ -1,12 +1,14 @@
-const progress = document.querySelector('.progress');
-  
-progress.addEventListener('input', function() {
+const progress = document.querySelector(".progress");
+
+progress.addEventListener("input", function () {
   const value = this.value;
-  this.style.background = `linear-gradient(to right, #24809E 0%, #24809E ${value}%, #fff ${value}%, white 100%)`
-})
-const progress = document.querySelector('.value__int');
-  
-progress.addEventListener('input', function() {
+  this.style.background = `linear-gradient(to right, #24809E 0%, #24809E ${value}%, #fff ${value}%, white 100%)`;
+});
+const progress = document.querySelector(".value__int");
+
+progress.addEventListener("input", function () {
   const value = this.value;
-  this.style.background = `linear-gradient(to right, #24809E 0%, #24809E ${value}%, #fff ${value}%, white 100%)`
-})
+  this.style.background = `linear-gradient(to right, #24809E 0%, #24809E ${value}%, #fff ${value}%, white 100%)`;
+});
+
+
